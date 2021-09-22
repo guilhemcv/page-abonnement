1963e6003f3def0f7b4a5d3a10e60e9a375dfd29
Guilhem CV <guilhemcvdev@outlook.com>
2021-09-22 21:33:42 +0200

    responsive

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gauche = document.querySelector(".gauche");
 const droite = document.querySelector(".droite");
 const centre = document.querySelector(".centre");
 const title = document.querySelector("h1");
-const texte = document.querySelector("p");
+
 
 let pricing = "month";
 
@@ -15,14 +15,13 @@ bouton.addEventListener("click", function(){
         tarifBasic.innerHTML = "$199,99";
         tarifProfessional.innerHTML = "$249,99";
         tarifMaster.innerHTML = "$399,99";
-        gauche.style.backgroundColor = "#F7DB69";
-        gauche.style.color = "#3B185F";
-        droite.style.backgroundColor = "#F7DB69";
-        droite.style.color = "#3B185F";
-        centre.style.backgroundColor = "#3B185F";
-        document.body.style.backgroundColor = "#A12568";   
-        title.style.color = "#F7DB69" ;
-        texte.style.color = "#F7DB69";
+       /*  gauche.style.backgroundColor = "#364F6B";
+        gauche.style.color = "#F7DB69";
+        droite.style.backgroundColor = "#364F6B";
+        droite.style.color = "#F7DB69";
+        centre.style.backgroundColor = "#3FC1C9";
+        centre.style.color = "#364F6B"; */
+        //document.body.style.backgroundColor = "#A12568";   
         pricing = "year";
     }
 
@@ -32,8 +31,13 @@ bouton.addEventListener("click", function(){
         tarifBasic.innerHTML = "$19,99";
         tarifProfessional.innerHTML = "$24,99";
         tarifMaster.innerHTML = "$39,99"; 
-
-       
+       /*  gauche.style.backgroundColor = "#3FC1C9";
+        gauche.style.color = "#364F6B";
+        droite.style.backgroundColor = "#3FC1C9";
+        droite.style.color = "#364F6B";
+        centre.style.backgroundColor = "#364F6B";
+        //document.body.style.backgroundColor = "#FEC260";  
+        centre.style.color = "#F7DB69";  */
         pricing = "month";
 
     }    
